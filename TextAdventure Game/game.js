d249f75355f0053ebceef60d6c3919b5325e87a8
Unknown <david.b586@stu.nebo.edu>
2019-01-23 10:27:12 -0700

    played with html a bit

---
+++ TextAdventure Game/game.js
@@ -0,0 +1,20 @@
+// Single line comment
+/*
+
+Multiline comment
+
+*/
+
+//document.write("<h1>Hey! <em>Same!</em></h1>");
+
+
+//alert("What are you doing user! Answer these questions to get in and you will find what I say about you!! Mwahahahahaha");
+
+//confirm("Do you like pokemon?");
+
+//prompt("What type of pokemon do you like?");
+
+var pc = prompt("What you be called?");
+
+confirm("So you be that one "+pc);
+
